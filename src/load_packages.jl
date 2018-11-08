9db27677f04fc63f140051657940714d4f4e317c
Benoit Pasquier <pasquieb@uci.edu>
2018-11-08 08:58:17 -0800

    using MAT.jl and JLD2.jl to load from .mat and save to .jld2

--- src/load_packages.jl
+++ src/load_packages.jl
@@ -1,16 +1,31 @@
-# Main script to plot paper figures
+# script to load all the packages needed
 
-# 1. Always load the packages
+# activate current Julia project
 using Pkg
 Pkg.activate(".")
+
+# use TransportMatrixTools package that I dev
 using TransportMatrixTools
-using SparseArrays, SuiteSparse, LinearAlgebra, Printf, DualNumbers
-using JLD2, Calculus, Optim
-# Packages for unitful parameters :)
-using FieldDefaults, Flatten, FieldMetadata, Unitful
-using Distributions
+
+# Matrix stuff
+using SparseArrays, SuiteSparse, LinearAlgebra
+
+# pretty print
+using Printf
+
+# Differentiating sutff
+using DualNumbers, Calculus
+
+# Loading and saving data
+using JLD2, MAT
+
+# Optimization package
+using Optim
+
+# Packages for parameters
+using FieldDefaults, Flatten, FieldMetadata, Unitful, Distributions
 import FieldDefaults: get_default
-import FieldMetadata: @units, units, @prior, prior, @description, description # 
+import FieldMetadata: @units, units, @prior, prior, @description, description
 import Flatten: flattenable
 
 
--- src/setup_OCIM1.1.jl
+++ src/setup_OCIM1.1.jl
@@ -1,5 +1,24 @@
+const spd = 24 * 60 * 60.0 # Think about using Unitful.jl
 
-@load (homedir() * "/.julia/v0.6/OCIMtools/data/OCIM1.1_CTL.jld2") wet3d grd T
+function load_OCIM_mat_file()
+    println("loading OCIM with MAT")
+    vars = matread(homedir() * "/Dropbox/OCIM1.1/CTL.mat")
+    T = -365spd * vars["output"]["TR"]
+    grd = vars["output"]["grid"]
+    wet3d = vars["output"]["M3d"]
+    return wet3d, grd, T
+end
+
+
+if isfile(joinpath(pwd(), "data", "OCIM1.1_CTL.jld2"))
+    println("loading OCIM with JLD2")
+    @load joinpath(pwd(), "data", "OCIM1.1_CTL.jld2") wet3d grd T
+else
+    wet3d, grd, T = load_OCIM_mat_file()
+    println("saving OCIM with JLD2")
+    @save joinpath(pwd(), "data", "OCIM1.1_CTL.jld2") wet3d grd T
+end
+
 
 # For standard use with OCIM
 const iwet = (LinearIndices(wet3d))[findall(!iszero, wet3d)] # replaces find(wet3d) :(
@@ -19,7 +38,6 @@ vmean(x) = v'x / vtot   # volume-weighted mean
 vmean(x::Vector{Complex{Float64}}) = transpose(v) * x / vtot   # volume-weighted mean
 const z = grd["ZT3d"][iwet]
 const ztop = grd["ZW3d"][iwet]
-const spd = 24 * 60 * 60.0 # Think about using Unitful.jl
 
 # load observations - Recode the fetching and regridding in Julia!
 @load (homedir() * "/.julia/v0.6/OCIMtools/data/OCIM1.1_SiObs.jld2") xobs
