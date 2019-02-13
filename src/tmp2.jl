ebbcf1d3a80753bb350ed4b7c2b04a7bc4c89b8e
Benoit Pasquier <pasquieb@uci.edu>
2019-02-13 11:10:16 +1100

    removed tmp from ignore files

---
+++ src/tmp2.jl
@@ -0,0 +1,26 @@
+# Main script to plot paper figures
+
+# 1. Always load the packages
+include("load_packages.jl")
+
+# Setup OCIM1.1 or toy model (comment/uncomment to use the one you need for now)
+
+# include("build_6BoxModel_circulation.jl")
+# using .SixBoxModel: T, wet3d, grd, spd, nwet, DIV, Iabove, ztop, DINobs , vnorm², maskEup, DINobsmean, Dvnorm²
+
+include("OCIM1.jl")
+using .OCIM1: T, wet3d, grd, spd, nwet, DIV, Iabove, ztop, DINobs , vnorm², maskEup, DINobsmean, Dvnorm²
+
+# The above must define everything needed by the functions
+
+# load biogeochmistry parameters
+include("bgc_parameters.jl")
+
+# load automatic differentiation stuff
+include("AutomaticDifferentiation.jl")
+
+# load biogeochmistry functions
+include("bgc_functions.jl")
+
+# load cost functions
+include("cost_functions.jl")
