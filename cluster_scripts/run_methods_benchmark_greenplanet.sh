ccc289d0926360b767e6ffa8c639d1462ac024bf
Benoit Pasquier <pasquieb@uci.edu>
2019-02-14 12:27:58 +1100

    fix julia version and cluster output

--- cluster_scripts/run_methods_benchmark_greenplanet.sh
+++ cluster_scripts/run_methods_benchmark_greenplanet.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=run_methods_benchmark
-#SBATCH --output=run_methods_benchmark.out
-#SBATCH --error=run_methods_benchmark.err
+#SBATCH --output=cluster_output/run_methods_benchmark.out
+#SBATCH --error=cluster_output/run_methods_benchmark.err
 #SBATCH --partition=brd2.4,has2.5,ilg2.3,m-c1.9,m-c2.2,nes2.8,sib2.9
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
@@ -10,7 +10,7 @@
 
 # Load the julia module
 ml purge
-ml julia/1.1
+ml julia/1.1.0
 
 # Go to the root folder on greenplanet
 cd /DFS-L/DATA/moore/pasquieb/Projects/FastBGCParameterOptimization
