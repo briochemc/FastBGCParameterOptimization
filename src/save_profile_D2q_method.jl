7af690a4dfb24ee83b5e5cff6f132d8027b7608c
Benoit Pasquier <pasquieb@uci.edu>
2019-02-19 09:40:30 +1100

    Added profile save/load code

--- src/save_profile_D2q_method.jl
+++ src/save_profile_D2q_method.jl
@@ -21,8 +21,23 @@ jld_file = joinpath(path_to_package_root, "data/D2q_profile_data.jld2")
 
 @save jld_file li lidict
 
+# Other method to save profile output
+r = Profile.retrieve();
+profile_file = joinpath(path_to_package_root, "data/D2q_profile_data.bin")
+profile_file_pointer = open(profile_file, "w")
+using Serialization
+serialize(profile_file_pointer, r)
+close(profile_file_pointer)
+
 # path_to_package_root = joinpath(splitpath(@__DIR__)[1:end-1]...)
 # jld_file = joinpath(path_to_package_root, "data/D2q_profile_data.jld2")
 # @load jld_file
 # ProfileView.view(li, lidict=lidict)
+
+# Code to plot the profile
+using ProfileView, Serialization
+profile_file = "data/D2q_profile_data.bin"
+f = open(profile_file)
+r = deserialize(f);
+ProfileView.view(r[1], lidict=r[2])
 
