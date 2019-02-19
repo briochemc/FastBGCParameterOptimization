93d96f01ddf9a8b7627cc04d9db8bc68790c980a
Benoit Pasquier <pasquieb@uci.edu>
2019-02-19 13:04:49 +1100

    commented plotting part

--- src/save_profile_D2q_method.jl
+++ src/save_profile_D2q_method.jl
@@ -8,18 +8,10 @@ optimize(q!, Dq!, D2q!, λ₀, NewtonTrustRegion(), opt)
 
 using Profile
 Profile.clear()  # in case we have any previous profiling data
-Profile.init(n = 10^7) # increase buffer size
+Profile.init(n = 10^7, delay = 0.1) # increase buffer size and reduce delay
 init.x, init.p = 1x₀, 3p₀
 J.fac, J.p = factorize(fJac(x₀, 3p₀)), 3p₀
 @profile optimize(q!, Dq!, D2q!, λ₀, NewtonTrustRegion(), opt)
-
-li, lidict = Profile.retrieve()
-using JLD2
-
-path_to_package_root = joinpath(splitpath(@__DIR__)[1:end-1]...)
-jld_file = joinpath(path_to_package_root, "data/D2q_profile_data.jld2")
-
-@save jld_file li lidict
 
 # Other method to save profile output
 r = Profile.retrieve();
@@ -35,9 +27,9 @@ close(profile_file_pointer)
 # ProfileView.view(li, lidict=lidict)
 
 # Code to plot the profile
-using ProfileView, Serialization
-profile_file = "data/D2q_profile_data.bin"
-f = open(profile_file)
-r = deserialize(f);
-ProfileView.view(r[1], lidict=r[2])
+#using ProfileView, Serialization
+#profile_file = "data/D2q_profile_data.bin"
+#f = open(profile_file)
+#r = deserialize(f);
+#ProfileView.view(r[1], lidict=r[2])
 
