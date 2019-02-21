096eb545f6a758b045c05139d4ae27221bda23b5
Benoit Pasquier <pasquieb@uci.edu>
2019-02-21 15:10:38 +1100

    some plotting

--- src/plot_methods_benchmark.jl
+++ src/plot_methods_benchmark.jl
@@ -3,6 +3,8 @@ using JLD2, Plots
 # Load the benchmark data to plot
 # TODO save the files with specific names to where it was run?
 @load "data/methods_benchmark_OCIM1_imac.jld2"
+# @load "data/methods_benchmark_data.jld2"
+msbd = methods_benchmark_data
 
 # TODO save the list_methods with the convergence results
 # TODO save the final params and cost to compare them too
@@ -12,6 +14,92 @@ list_methods = [
     ("CSDq", :q!,  :Dq!, :CSDDq!)
     ("FDDq", :q!,  :Dq!,  :FDDq!)
 ]
+
+fields_with_time = [
+    "q",
+    "Dq",
+    "FDq",
+    "CSDq",
+    "ADq",
+    "D2q",
+    "FD2q",
+    "FDDq",
+    "CSDDq",
+    "ADDq",
+    "AD2q",
+    "factorize",
+    "backsolve"
+]
+
+start_times = Dict()
+for method in list_methods
+    #for run_str in ["_1strun", "_2ndrun"]
+    for run_str in ["_1strun"]
+        field_name = method[1] * run_str
+        mbd = msbd[field_name]
+        #println(field_name)
+        #println(minimum([first(mbd[f][1]) for f in fields_with_time if has_timer(mbd, f)]))
+        #println([msbd[field_name][ftime][1] for ftime in fields_with_time if ~isempty(msbd[field_name][ftime][1])])
+        push!(start_times, field_name => minimum([first(mbd[f][1]) for f in fields_with_time if has_timer(mbd, f)]))
+    end
+end
+
+has_timer(mbd, f) = haskey(mbd, f) && ~isempty(mbd[f][1])
+
+
+end_costs = Vector{Float64}()
+for method in list_methods
+    for run_str in ["_1strun"]
+    #for run_str in ["_1strun", "_2ndrun"]
+        field_name = method[1] * run_str
+        push!(end_costs, minimum(methods_benchmark_data[field_name]["qvalues"]))
+    end
+end
+using Statistics
+end_cost = mean(end_costs)
+
+p = plot()
+for (i, method) in enumerate(list_methods)
+    for run_str in ["_1strun"]
+        field_name = method[1] * run_str
+        x = (methods_benchmark_data[field_name]["q"][2] .- start_times[field_name]) ./ 60
+        y = abs.(methods_benchmark_data[field_name]["qvalues"] .- end_cost)
+        plot!(p, x, y, yaxis = :log, label = field_name)
+        xlabel!(p, "time (min)")
+        ylabel!(p, "cost function error")
+        display(p)
+    end
+end
+display(p)
+
+
+###
+#
+# Start Checking time spent in each phase
+#
+###
+markers = [:o, :d, :s, :v]
+
+p = plot()
+for (i, method) in enumerate(list_methods)
+    field_name = method[1] * "_1strun"
+    mbd = msbd[field_name]
+    for f in fields_with_time
+        f == "q" || isempty(mbd[f][1]) ? continue : nothing
+        println(f)
+        tictocs = mbd[f]
+        label = method[1] * " - " * f
+        plot!(p, cumsum(tictocs[2] - tictocs[1])/60, axis = :log, label = label, marker = markers[i])
+        display(p)
+    end
+end
+
+
+####
+#
+# End Checking time spent in each phase
+#
+####
 
 min_ys = Vector{Float64}()
 for (i, method) in enumerate(list_methods)
@@ -41,12 +129,13 @@ display(p)
 # savefig("fig/methods_benchmark_logDeltaq_vs_time.pdf")
 
 function mystep(tbefore, tafter, vals)
-    x, y = Vector{Float64}(), Vector{Float64}()
+    x, y, oldv = Vector{Float64}(), Vector{Float64}(), NaN
     for (tb, ta, v) in zip(tbefore, tafter, vals)
         push!(x, tb)
-        push!(y, v)
+        push!(y, oldv)
         push!(x, ta)
         push!(y, v)
+        oldv = v
     end
     return x, y
 end
