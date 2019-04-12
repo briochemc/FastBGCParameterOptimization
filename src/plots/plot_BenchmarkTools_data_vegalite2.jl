d99a32e4f7155e018d21f1c0e33ef9480c615cdd
Benoit Pasquier <pasquieb@uci.edu>
2019-04-12 14:52:26 +1000

    update plots

--- src/plots/plot_BenchmarkTools_data_vegalite2.jl
+++ src/plots/plot_BenchmarkTools_data_vegalite2.jl
@@ -1,4 +1,4 @@
-using JLD2, BenchmarkTools, VegaLite, DataFrames
+using JLD2, BenchmarkTools, VegaLite, DataFrames, Statistics
 
 #@load "data/BenchmarkTools_data.jld2"
 path_to_package_root = joinpath(splitpath(@__DIR__)[1:end-2]...)
@@ -6,10 +6,18 @@ str_out = "_default"
 jld_file = joinpath(path_to_package_root, "data", "BenchmarkTools_data" * str_out * ".jld2")
 @load jld_file results list_functions
 
+list_∇ᵏf̂ = [:f̂, :∇f̂, :∇²f̂]
+list_∇ᵏf = [:f, :∇f, :∇²f]
+list_methods = [:F1, :DUAL, :CSD, :FD1, :HYPER, :FD2]
+list_m = ["F-1", "DUAL", "CSD", "FD1", "HYPER", "FD2"]
+
 strings = string.(list_functions)
 f̂_list = strings[[occursin("_f̂", s) for s in strings]]
 ∇f̂_list = strings[[occursin("_∇f̂", s) for s in strings]]
 ∇²f̂_list = strings[[occursin("_∇²f̂", s) for s in strings]]
+f̂_to_f(x::String) = replace(replace(x, "f̂" => "f"), "F1" => "F-1")
+f̂_to_f(x::Array) = [f̂_to_f(e) for e in x]
+
 
 function results_to_df(results, list)
     df = DataFrame(
@@ -23,9 +31,9 @@ function results_to_df(results, list)
         df = push!(
             df,
             Dict(
-                :method => m,
-                :fgh => fun,
-                :time => myround(results[f].times[] * 1e-9),
+                :method => f̂_to_f(string(m, " ", fun)),
+                :fgh => f̂_to_f(string(fun)),
+                :time => results[f].times[] * 1e-9,
                 :allocs => results[f].allocs[] * 2^-27
             )
         )
@@ -33,15 +41,38 @@ function results_to_df(results, list)
     return df
 end
 
-myround(x) = x ≥ 100 ? round(x) : round(x*10) / 10
-
 
 df1 = results_to_df(results, f̂_list)
+mymean(x) = mean(x)
+function mymean(x::Array{String})
+    if x[1] == "f"
+        return "f"
+    elseif x[1] == "∇f"
+        return "∇f"
+    elseif occursin("∇f", x[1])
+        return "Analytical ∇f"
+    else
+        return "Analytical f"
+    end
+end
+push!(df1, colwise(mymean, df1))
+filter!(row -> row[:method] == "Analytical f", df1)
+
 df2 = results_to_df(results, ∇f̂_list)
+df2a = filter(row -> row[:method] ∈ ["DUAL ∇f", "CSD ∇f", "FD1 ∇f"], df2)
+df2b = filter(row -> row[:method] ∉ ["DUAL ∇f", "CSD ∇f", "FD1 ∇f"], df2)
+push!(df2a, colwise(mymean, df2a))
+filter!(row -> row[:method] == "Analytical ∇f", df2a)
+df2 = vcat(df2a, df2b)
+
 df3 = results_to_df(results, ∇²f̂_list)
 
-p1 = df1 |> @vlplot(
-    title={text="(a)", anchor="start"},
+df = vcat(df1, df2, df3)
+myround(x) = x ≥ 100 ? round(x) : round(x*10) / 10
+df[:time] .= myround.(df[:time])
+
+p = df |> @vlplot(
+    #title={text="(a)", anchor="start"},
     encoding={
         x={:time, title="Computation time (seconds)"},
         y={:method, sort=true},
@@ -64,80 +95,10 @@ p1 = df1 |> @vlplot(
     }
 )
 
-p2 = df2 |> @vlplot(
-    title={text="(b)", anchor="start"},
-    encoding={
-        x={:time, title="Computation time (seconds)", scale={domain=[0,2500]}},
-        y={:method, sort=true},
-        color={:fgh, title="", scale={scheme="set2"}, legend={orient="top-right"}},
-        text={:time},
-    },
-    resolve={
-        scale={
-            y ="shared"
-        }
-    }
-) +
-@vlplot(:bar) +
-@vlplot(
-    mark={
-        :text,
-        align=:left,
-        baseline=:middle,
-        dx=5
-    }
-)
-
-p3 = df3 |> @vlplot(
-    title={text="(b)", anchor="start"},
-    encoding={
-        x={:time, title="Computation time (seconds)", scale={domain=[0,2500]}},
-        y={:method, sort=true},
-        color={:fgh, title="", scale={scheme="set2"}, legend={orient="top-right"}},
-        text={:time},
-    },
-    resolve={
-        scale={
-            y ="shared"
-        }
-    }
-) +
-@vlplot(:bar) +
-@vlplot(
-    mark={
-        :text,
-        align=:left,
-        baseline=:middle,
-        dx=5
-    }
-)
-
-p = [p1; p2; p3]
-
 display(p)
 
 # print to PDF
 pdf_file = joinpath(path_to_package_root, "fig",  "BenchmarkTools_data" * str_out * ".pdf")
 save(pdf_file, p)
 
-
-
-
-
-#p = plot()
-#for (i, k) in enumerate(mykeys)
-#    println(float(i), " - ", results[k].times[] * 1e-9)
-#    x = results[k].times[] * 1e-9 # time in seconds
-#    mycolor = mycolors[2]
-#    k == "q!" ? mycolor = mycolors[3] : nothing
-#    k == "Dq!" ? mycolor = mycolors[1] : nothing
-#    plot!(p, (i, x), seriestype=:bar, label=k[1:end-1], color=mycolor)
-#end
-#plot!(p, yticks=collect(0:60:500))
-#plot!(p, ylabel="Time (seconds)")
-#plot!(p, xticks=(1:6,map(x->x[1:end-1], mykeys)))
-#plot!(p, legend=nothing)
-#display(p)
-#
-#savefig(p, "fig/BenchmarkTools_data_katana2.pdf")
 
--- src/plots/plot_TimerOutputs_vegalite.jl
+++ src/plots/plot_TimerOutputs_vegalite.jl
@@ -1,37 +1,24 @@
 # Load plotting package
 using VegaLite, DataFrames # for stacked bars
 
-
-
-
-
-
-# using LaTeXStrings # for LaTeX labels
-#using ColorBrewer # for better colors
-
-
 # Load data
 using JLD2
-path_to_package_root = joinpath(splitpath(@__DIR__)[1:end-1]...)
+path_to_package_root = joinpath(splitpath(@__DIR__)[1:end-2]...)
 str_out = "_default"
 jld_file = joinpath(path_to_package_root, "data", "TimerOutputs_data" * str_out * "_katana" * ".jld2")
 @load jld_file timers
 
 translate_for_legend = Dict("f"=>"objective", "∇f"=>"gradient", "∇²f"=>"Hessian")
 
-# Reshape data into a DataFrame
-list_methods1 = ["F-1", "old F-0", "old F-1", "DUAL", "FD1", "CSD"]
-list_methods2 = ["F-1", "old F-0", "old F-1", "HYPER", "FD2"]
-list_methods1_old = ["OF1", "F0", "F1", "DUAL", "FD1", "CSD"]
-list_methods2_old = ["OF1", "F0", "F1", "HYPER", "FD2"]
-# Without old methods
-list_methods1 = ["F-1", "DUAL", "FD1", "CSD"]
-list_methods2 = ["F-1", "HYPER", "FD2"]
-list_methods1_old = ["OF1", "DUAL", "FD1", "CSD"]
-list_methods2_old = ["OF1", "HYPER", "FD2"]
+list_∇ᵏf̂ = [:f̂, :∇f̂, :∇²f̂]
+list_∇ᵏf = [:f, :∇f, :∇²f]
+list_methods = [:F1, :DUAL, :CSD, :FD1, :HYPER, :FD2]
+list_m = ["F-1", "DUAL", "CSD", "FD1", "HYPER", "FD2"]
 
 
-function timer_to_DataFrame(timers::Dict, l, l_old)
+
+
+function timer_to_DataFrame(timers::Dict, I)
     df = DataFrame(
         method = Array{String}(undef, 0),
         fgh = Array{String}(undef, 0),
@@ -39,17 +26,17 @@ function timer_to_DataFrame(timers::Dict, l, l_old)
         allocs = Array{Float64}(undef, 0),
         ncalls = Array{Int64}(undef, 0)
     )
-    for (ik, k) in enumerate(l_old)
-        t = timers[k]
-        for k2 in ["f", "∇f", "∇²f"]
+    for (m, m2) in zip(list_methods[I], list_m[I])
+        t = timers[string(m)]
+        for (∇ᵏf̂, ∇ᵏf) in zip(list_∇ᵏf̂, list_∇ᵏf)
             push!(
                 df,
                 Dict(
-                    :method => l[ik],
-                    :fgh => k2,
-                    :time => t[k2]["time"] * 1e-9,
-                    :allocs => t[k2]["allocs"] * 2^-30,
-                    :ncalls => t[k2]["ncalls"]
+                    :method => string(m2),
+                    :fgh => string(∇ᵏf),
+                    :time => t[string(m, "_", ∇ᵏf̂)]["time"] * 1e-9,
+                    :allocs => t[string(m, "_", ∇ᵏf̂)]["allocs"] * 2^-30,
+                    :ncalls => t[string(m, "_", ∇ᵏf̂)]["ncalls"]
                 )
             )
         end
@@ -57,10 +44,12 @@ function timer_to_DataFrame(timers::Dict, l, l_old)
     return df
 end
 
-df1 = timer_to_DataFrame(timers, list_methods1, list_methods1_old)
-df2 = timer_to_DataFrame(timers, list_methods2, list_methods2_old)
+df1 = timer_to_DataFrame(timers, 1:4)
+df2 = timer_to_DataFrame(timers, [1,5,6])
 
-
+orderf = [string(f) for f in list_∇ᵏf]
+order1 = [string(m) for m in list_m[1:4]]
+order2 = [string(m) for m in list_m[[1,5,6]]]
 
 ptime1 = df1 |> @vlplot(
     width=300,
@@ -69,10 +58,10 @@ ptime1 = df1 |> @vlplot(
     mark={
         :bar,
     },
-    x={"sum(time)", title="Computation time (seconds)", scale={domain=[0,3500]}},
-    y={:method, sort=list_methods1},
-    color={:fgh, title="", scale={scheme="set2"}, legend={orient="top-right", sort=["f", "∇f", "∇²f"]}},
-    order={field=:method, sort=["f", "∇f", "∇²f"]}
+    x={"sum(time)", title="Computation time (seconds)", scale={domain=[0,3000]}},
+    y={:method, sort=order1},
+    color={:fgh, title="", scale={scheme="set2"}, legend={orient="top-right", sort=orderf}},
+    order={field=:method, sort=orderf}
 )
 
 ptime2 = df2 |> @vlplot(
@@ -83,8 +72,8 @@ ptime2 = df2 |> @vlplot(
         :bar,
     },
     x={"sum(time)", title="Computation time (seconds)"},
-    y={:method, sort=list_methods2},
-    color={:fgh, title="", scale={scheme="set2"}, legend={orient="top-right", sort=["f", "∇f", "∇²f"]}},
+    y={:method, sort=order2},
+    color={:fgh, title="", scale={scheme="set2"}, legend={orient="top-right", sort=orderf}},
     order={field=:method, sort="count"}
 )
 
@@ -97,108 +86,4 @@ pdf_file_time = joinpath(path_to_package_root, "fig", "TimerOutputs_time.pdf")
 save(pdf_file_time, ptime)
 
 
-#pmem = df |> @vlplot(
-#    :bar,
-#    x={"sum(allocs)", title="Memory allocations (GiB)"},
-#    y=:method,
-#    color={:fgh, title=""}
-#)
-#
-## print to PDF
-#pdf_file_mem = joinpath(path_to_package_root, "fig", "TimerOutputs_mem.pdf")
-#save(pdf_file_mem, pmem)
-#
-#pncalls = df |> @vlplot(
-#    :bar,
-#    x={"sum(ncalls)", title="Number of calls"},
-#    y=:method,
-#    color={:fgh, title=""}
-#)
-#
-## print to PDF
-#pdf_file_ncalls = joinpath(path_to_package_root, "fig", "TimerOutputs_ncalls.pdf")
-#save(pdf_file_ncalls, pncalls)
 
-#savefig(p, "fig/TimerOutputs_data_test.pdf")
-#
-#display(p)
-
-## Data from printed output copied below
-#TimerOutputs = [
-#  # CSDDq  # ADDq       # FDDq         # D2q
-#     229.0    223.0       214.0         223.0        # Dq
-#    2978.0   1551.0      1065.0         81.3       # D2q
-#]
-#
-#p = groupedbar(
-#    color=ColorBrewer.palette("Set2", 3)[1:2]',
-#    TimerOutputs',
-#    bar_position=:stack,
-#    labels=["Gradient", "Hessian"],
-#    legend=:top,
-#    xticks=(1:4, ["Complex step", "Dual numbers", "Finite difference", "This paper"]),
-#    xlabel="Method",
-#    ylabel="Time (seconds)",
-#    yminorticks=collect(0:60:3600),
-#    yticks=collect(0:300:3600)
-#)
-#
-#display(p)
-#
-#savefig(p, "fig/TimerOutputs_data_katana.pdf")
-#
-#display(p)
-#=
-
-DATA FROM KATANA Feb 26 2019
-
- ───────────────────────────────────────────────────────────────────────
-                                Time                   Allocations
-                        ──────────────────────   ───────────────────────
-    Tot / % measured:        1773s / 100%             540GiB / 100%
-
- Section        ncalls     time   %tot     avg     alloc   %tot      avg
- ───────────────────────────────────────────────────────────────────────
- Trust Region        1    1773s   100%   1773s    540GiB  100%    540GiB
-   D2q (dual)        5    1551s  87.4%    310s    471GiB  87.3%  94.2GiB
-   Dq                6     223s  12.6%   37.1s   68.7GiB  12.7%  11.5GiB
-   q                 6   15.6ms  0.00%  2.61ms   45.8MiB  0.01%  7.64MiB
- ─────────────────────────────────────────────────────────────────────── ──────────────────────────────────────────────────────────────────────────
-                                   Time                   Allocations
-                           ──────────────────────   ───────────────────────
-     Tot / % measured:          3207s / 100%             868GiB / 100%
-
- Section           ncalls     time   %tot     avg     alloc   %tot      avg
- ──────────────────────────────────────────────────────────────────────────
- Trust Region           1    3207s   100%   3207s    868GiB  100%    868GiB
-   D2q (complex)        5    2978s  92.9%    596s    797GiB  91.8%   159GiB
-   Dq                   6     229s  7.14%   38.2s   71.2GiB  8.20%  11.9GiB
-   q                    6   15.3ms  0.00%  2.55ms   45.8MiB  0.01%  7.64MiB
- ────────────────────────────────────────────────────────────────────────── ──────────────────────────────────────────────────────────────────────────────
-                                       Time                   Allocations
-                               ──────────────────────   ───────────────────────
-       Tot / % measured:            1278s / 100%             384GiB / 100%
-
- Section               ncalls     time   %tot     avg     alloc   %tot      avg
- ──────────────────────────────────────────────────────────────────────────────
- Trust Region               1    1278s   100%   1278s    384GiB  100%    384GiB
-   D2q (finite-diff)        5    1065s  83.3%    213s    316GiB  82.1%  63.1GiB
-   Dq                       6     214s  16.7%   35.6s   68.6GiB  17.8%  11.4GiB
-   q                        6   13.9ms  0.00%  2.32ms   45.8MiB  0.01%  7.64MiB
- ──────────────────────────────────────────────────────────────────────────────
-
- ───────────────────────────────────────────────────────────────────────
-                                Time                   Allocations
-                        ──────────────────────   ───────────────────────
-    Tot / % measured:         304s / 100%            90.2GiB / 100%
-
- Section        ncalls     time   %tot     avg     alloc   %tot      avg
- ───────────────────────────────────────────────────────────────────────
- Trust Region        1     304s   100%    304s   90.2GiB  100%   90.2GiB
-   Dq                6     223s  73.3%   37.2s   68.7GiB  76.2%  11.5GiB
-   D2q (mine)        5    81.3s  26.7%   16.3s   21.5GiB  23.8%  4.29GiB
-   q                 6   15.6ms  0.01%  2.60ms   45.8MiB  0.05%  7.64MiB
- ───────────────────────────────────────────────────────────────────────
-
-
-=#
