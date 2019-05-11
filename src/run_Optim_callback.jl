45ac53bc007ecd53455d6d54c9f15a69f64f563a
Benoit Pasquier <pasquieb@uci.edu>
2019-05-11 17:16:23 +1000

    fix Optim callback run with OCIM

--- src/run_Optim_callback.jl
+++ src/run_Optim_callback.jl
@@ -1,4 +1,9 @@
 
-include("setup_with_OCIM.jl")
+include("load_packages.jl")
+
+# Define the transport matrix for the circulation
+Circulation = OCIM # from AIBECS
+
+include("setup.jl")
 
 include("save_Optim_callback_data.jl")
